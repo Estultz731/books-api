cde8d7ced76a232e1834ccf51e9e778cce978ecc
Estultz731 <elizablue731@gmail.com>
2019-11-01 18:29:28 -0400

    added store method and controller

--- app/Http/Controllers/AuthorsController.php
+++ app/Http/Controllers/AuthorsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Author;
 
+use Illuminate\Http\Request;
+
 class AuthorsController extends Controller
 {
   public function index()
@@ -25,6 +27,18 @@ class AuthorsController extends Controller
     Author::find($id)->delete();
     return response()
       ->json('Ok');
+  }
+
+  public function store(Request $request)
+  {
+    //Get the first name and last name from the request 
+    $firstname = $request->input('first_name');
+    $lastname = $request->input('last_name');
+    //Create a new author using the Author model
+    $author = Author::create(['first_name' => $firstname, 'last_name' => $lastname]);
+    //Return the model as json
+    return response()
+      ->json($author);
   }
 }
 
