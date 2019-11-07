b00ffdcaf16585436ea002ad7c8f7267fb15e1b8
Estultz731 <elizablue731@gmail.com>
2019-11-07 11:29:08 -0500

    added styles to index view

--- resources/views/authors/index.blade.php
+++ resources/views/authors/index.blade.php
@@ -6,6 +6,22 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="">
+    <style>
+      table {
+        width: 100%;
+        border: 1px solid #d9d9dd;
+      }
+
+      th, td {
+        text-align: left;
+        font-family: arial;
+        padding: 10px;
+      }
+
+      tr:nth-child(odd) {
+        background-color: #d9d9dd;
+      }
+    </style>
   </head>
   <body>
    <table>
