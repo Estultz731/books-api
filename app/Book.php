150c0ea606f723b0762546e933f997dff30613c3
Estultz731 <elizablue731@gmail.com>
2019-11-27 11:54:25 -0500

    added genre model and many-to-many relationships for genre model and book model

--- app/Book.php
+++ app/Book.php
@@ -17,4 +17,9 @@ class Book extends Model
     {
         return $this->belongsTo('App\Author');
     }
+
+    public function genres()
+    {
+        return $this->belongsToMany('App\Genre');
+    }
 }
